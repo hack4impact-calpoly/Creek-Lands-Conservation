11fb1685ee52676d7e399618e8699dceb5d91849
kjdiaz508 <diazkevin508@gmail.com>
2025-01-19 13:01:54 -0800

    button with my name on it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Button>Home</Button>
+      <Link href="/kjdiaz508">
+        <button className="rounded-lg bg-blue-500 px-6 py-2 text-lg font-medium text-white hover:bg-blue-600">
+          Kevin Diaz
+        </button>
+      </Link>
     </main>
   );
 }
