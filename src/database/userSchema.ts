b2e92cd4b9faf800f4d16eab2ddd8fbe77283179
HanYu Wu <hanyuwu04@gmail.com>
2025-02-01 15:59:57 -0800

    updated event and user schema

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,9 +1,59 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-//! Example user schema. Not guaranteed to work
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+export interface IChild {
+  childID: mongoose.Types.ObjectId; // Unique identifier for each child
+  name: string;
+  birthday: Date;
+  gender: "Male" | "Female" | "Non-binary" | "Prefer Not to Say" | "";
+  imageUrl: { type: String; default: "" };
+  waiversSigned: mongoose.Types.ObjectId[];
+  registeredEvents: mongoose.Types.ObjectId[];
+}
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export interface IUser extends Document {
+  clerkID: string;
+  userRole: "user" | "admin" | "donator";
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: "Male" | "Female" | "Non-binary" | "Prefer Not to Say" | "";
+  birthday: Date;
+  imageUrl?: string;
+  children: IChild[];
+  registeredEvents: mongoose.Types.ObjectId[];
+  waiversSigned: mongoose.Types.ObjectId[];
+}
+
+const childSchema = new Schema<IChild>(
+  {
+    childID: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    name: { type: String },
+    birthday: { type: Date },
+    gender: { type: String, enum: ["Male", "Female", "Non-binary", "Prefer Not to Say"], default: "" },
+    imageUrl: { type: String, default: "" },
+    registeredEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+    waiversSigned: [{ type: mongoose.Schema.Types.ObjectId, ref: "Waiver" }],
+  },
+  { _id: false },
+);
+
+const userSchema = new Schema<IUser>(
+  {
+    clerkID: { type: String, required: true, unique: true },
+    userRole: { type: String, enum: ["user", "admin", "donator"] },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    gender: { type: String, enum: ["Male", "Female", "Non-binary", "Prefer Not to Say", ""], default: "" },
+    imageUrl: { type: String, default: "" },
+    birthday: { type: Date },
+    children: { type: [childSchema], default: [] },
+    registeredEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+    waiversSigned: [{ type: mongoose.Schema.Types.ObjectId, ref: "Waiver" }],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.models["users"] || mongoose.model<IUser>("users", userSchema);
