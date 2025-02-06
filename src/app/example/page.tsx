e1bd35c358a2f0a24a22ef5bf66597a43bbc6907
HanYu Wu <hanyuwu04@gmail.com>
2025-02-05 16:56:47 -0800

    resetting example page

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,17 +1,3 @@
-import EventCard from "@/components/EventComponent/EventCard";
-
 export default function Page() {
-  return (
-    <div className="container">
-      <div>This is an example page using App Router!</div>
-      <div className="p-4">
-        <EventCard
-          title="SLO Wild Hike: Stenner Creek"
-          date="Saturday 4/6/25"
-          time="10–12pm"
-          onMoreInfo="More info about Stenner Creek hike"
-        />
-      </div>
-    </div>
-  );
+  return <div>This is an example page using App Router!</div>;
 }
