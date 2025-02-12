9ad566a92f3d6b1962a50c8e3f7d9eef1a9b43f7
HanYu Wu <hanyuwu04@gmail.com>
2025-02-11 18:08:19 -0800

    configuring image domain with s3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: ["creeklandsconservation.s3.us-west-1.amazonaws.com"],
+  },
+  // ...other Next.js config options
+};
