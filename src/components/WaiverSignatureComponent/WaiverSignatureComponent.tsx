58f5b7c0e5b12b2855d3622c253ada562f9e58c8
kjdiaz508 <diazkevin508@gmail.com>
2025-02-13 23:46:29 -0800

    feat: added a waiver signature status component

---
+++ src/components/WaiverSignatureComponent/WaiverSignatureComponent.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+interface WaiverSignatureProps {
+  eventName: string;
+  eventDate: Date;
+  eventTime: string;
+  signed: boolean;
+  signatureDate?: Date;
+  singByDate?: Date;
+  onViewWaiver: () => void;
+  onSignWaiver?: () => void;
+}
+
+const getDateString = (date: Date) =>
+  new Intl.DateTimeFormat("en-US", { month: "numeric", day: "numeric", year: "2-digit" }).format(date);
+const getWeekDay = (date: Date) => new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+
+const WaiverSignatureComponent: React.FC<WaiverSignatureProps> = ({
+  eventName,
+  eventDate,
+  eventTime,
+  signed,
+  signatureDate,
+  singByDate,
+  onViewWaiver,
+  onSignWaiver,
+}) => {
+  const eventDay = getWeekDay(eventDate);
+  const eventDateString = getDateString(eventDate);
+  const signatureDateString = signatureDate && getDateString(signatureDate);
+  const singByDateString = singByDate && getDateString(singByDate);
+
+  return (
+    <Card className="flex w-full items-center justify-between gap-3 rounded-lg bg-gray-100 p-4 shadow-md">
+      <div className="flex flex-wrap items-center gap-2 md:gap-4 xl:gap-10">
+        <div className="truncate text-sm font-semibold sm:text-base lg:text-lg">{eventName}</div>
+        <div className="flex flex-row items-center gap-1 text-sm sm:text-base">
+          <div className="hidden text-gray-600 sm:block">{eventDay}</div>
+          <div className="text-gray-600">{eventDateString}</div>
+        </div>
+        <div className="hidden text-gray-600 md:block">{eventTime}</div>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex items-center gap-2 sm:gap-4">
+        <Button
+          onClick={signed ? onViewWaiver : onSignWaiver}
+          variant="default"
+          className={`gap-1 rounded-lg px-2 py-3 text-sm font-semibold sm:px-3 sm:py-4 sm:text-base lg:px-4 lg:py-6 lg:text-lg ${
+            signed ? "bg-green-500 text-white" : "bg-red-500 text-white"
+          }`}
+        >
+          {signed ? (
+            `✅ Signed ${signatureDateString}`
+          ) : (
+            <>
+              ⚠️ Needs Signature
+              {singByDateString && <span className="hidden md:inline">by {singByDateString}</span>}
+              <span className="inline md:hidden">→</span>
+            </>
+          )}
+        </Button>
+
+        {/* View Waiver Button (Only visible on lg+ screens) */}
+        <Button
+          className="hidden rounded-lg bg-gray-500 px-4 py-6 text-base text-white transition hover:bg-gray-600 lg:flex"
+          onClick={signed ? onViewWaiver : onSignWaiver}
+        >
+          {signed ? "Click here to View Waiver" : "Click here to View and Sign Waiver"}
+        </Button>
+      </div>
+    </Card>
+  );
+};
+
+export default WaiverSignatureComponent;
