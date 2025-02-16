7f52c37a3bc7c68cf8179887f8eceec3f9c0e818
HanYu Wu <hanyuwu04@gmail.com>
2025-02-15 17:07:58 -0800

    removing comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 const isOnboardingRoute = createRouteMatcher(["/onboarding", "/onboarding/children"]);
-const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/clerk"]); //excluding webhook from auth to allow server sending webhooks
+const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/clerk"]);
 
 export default clerkMiddleware(async (auth, req: NextRequest) => {
   const { userId, sessionClaims, redirectToSignIn } = await auth();
