f5bb835f719fb16f3121ce03980144ef00a11f87
kjdiaz508 <diazkevin508@gmail.com>
2025-02-21 16:58:56 -0800

    using clerk route matching for admin routes

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
     };
 
     // Change this to user in production
-    let role = "admin";
+    const role = "admin";
     const client = await clerkClient();
 
     try {
