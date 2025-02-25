715562a051325d275f4c81e4d9508357b78c154d
HanYu Wu <hanyuwu04@gmail.com>
2025-02-24 20:29:58 -0800

    removing userID component and putting user component in user profile page

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PersonalInfo from "@/components/UserComponent/UserComponent";
+
+export default async function UserProfile() {
+  return (
+    <main>
+      <PersonalInfo></PersonalInfo>
+    </main>
+  );
+}
