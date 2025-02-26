6e2146c1c8521ff01332ee5a240ef50cefd81733
Sid Meka <sidmekarao@gmail.com>
2025-02-26 11:31:37 -0800

    phone and address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@
 
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
-
+import PersonalInfo from "@/components/UserComponent/UserComponent";
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Button>Home</Button>
+      <PersonalInfo></PersonalInfo>
     </main>
   );
 }
