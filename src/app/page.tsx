9f1909c23d68f8fda5592be9c7b5fd6223d422f2
HanYu Wu <hanyuwu04@gmail.com>
2025-02-26 13:02:33 -0800

    removing personal component from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@
 
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
-import PersonalInfo from "@/components/UserComponent/UserComponent";
+
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Button>Home</Button>
-      <PersonalInfo></PersonalInfo>
     </main>
   );
 }
