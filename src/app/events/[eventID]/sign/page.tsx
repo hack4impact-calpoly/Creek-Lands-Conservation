648cfc3ba186b98e9691363055a01803fba32b32
Ida Voong <idavoong@gmail.com>
2025-06-05 00:34:11 -0700

    stripe payment

--- src/app/events/[eventID]/sign/page.tsx
+++ src/app/events/[eventID]/sign/page.tsx
@@ -16,21 +16,38 @@ type Participant = {
 export default function WaiverSignPage({ params }: { params: { eventID: string } }) {
   const router = useRouter();
   const [participants, setParticipants] = useState<Participant[] | null>(null);
+  const [fee, setFee] = useState<number | null>(null);
+  const [eventTitle, setEventTitle] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
 
   useEffect(() => {
     try {
       // using localstorage to avoid huge query strings in the URL.
       const stored = localStorage.getItem("waiverParticipants");
+      console.log("Stored participants:", stored);
       if (!stored) {
         router.push("/"); // fallback to home if no data
         return;
       }
       const parsed = JSON.parse(stored);
       if (!Array.isArray(parsed)) throw new Error("Invalid participant data");
+
       setParticipants(parsed);
     } catch (error) {
       console.error("Failed to load participants from localStorage:", error);
       router.push("/"); // fallback again
+    }
+
+    try {
+      const eventFee = localStorage.getItem("eventFee");
+      const eventTitle = localStorage.getItem("eventTitle");
+      const id = localStorage.getItem("userId");
+
+      setFee(eventFee ? parseFloat(eventFee) : null);
+      setEventTitle(eventTitle);
+      setUserId(id);
+    } catch (error) {
+      console.log("no fee, title, or userId in localStorage, this is fine if the event doesn't require payment");
     }
   }, [router]);
 
@@ -43,8 +60,51 @@ export default function WaiverSignPage({ params }: { params: { eventID: string }
     router.push("/");
   };
 
+  const handlePayment = async () => {
+    localStorage.removeItem("waiverParticipants");
+    localStorage.removeItem("eventFee");
+    localStorage.removeItem("eventTitle");
+    localStorage.removeItem("userId");
+
+    try {
+      const parsedFee = Math.round(fee ? fee * 100 : 0); // Convert to cents
+      const eventData = {
+        title: eventTitle,
+        fee: parsedFee, // Fee in cents
+        quantity: participants?.length,
+        eventId: params.eventID,
+        attendees: participants?.map((participants) => participants.userID), // only pass userID
+        userId: userId,
+      };
+
+      const res = await fetch("/api/stripe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(eventData),
+      });
+
+      const data = await res.json();
+      if (data.sessionUrl) {
+        window.location.href = data.sessionUrl;
+      } else {
+        console.error("Stripe error:", data.error);
+      }
+    } catch (error) {
+      console.error("Payment error:", error);
+      toast({
+        title: "Payment Error",
+        description: "There was an error processing your payment. Please try again later.",
+        variant: "destructive",
+      });
+    }
+  };
+
   return participants ? (
-    <WaiverSignatureForm eventId={params.eventID} participants={participants} onAllSigned={handleAllSigned} />
+    <WaiverSignatureForm
+      eventId={params.eventID}
+      participants={participants}
+      onAllSigned={fee === 0 ? handleAllSigned : handlePayment}
+    />
   ) : (
     <WaiverSignatureFormSkeleton />
   );
