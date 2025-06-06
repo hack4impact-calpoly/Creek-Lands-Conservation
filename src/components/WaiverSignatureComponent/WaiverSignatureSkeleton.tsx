a0e0a0a24192e5770af80010b081dbe6f70dd471
HanYu-Wu04 <hanyuwu04@gmail.com>
2025-06-05 18:36:29 -0700

    updating waiver page

--- src/components/WaiverSignatureComponent/WaiverSignatureSkeleton.tsx
+++ src/components/WaiverSignatureComponent/WaiverSignatureSkeleton.tsx
@@ -1,8 +1,7 @@
-import type React from "react";
 import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
-const WaiverSignatureSkeleton: React.FC = () => {
+export default function WaiverSignatureSkeleton() {
   return (
     <Card className="flex w-full flex-col gap-3 rounded-lg p-4 shadow-sm sm:flex-row sm:items-center sm:justify-between">
       <div className="flex w-full flex-col gap-2">
@@ -12,17 +11,15 @@ const WaiverSignatureSkeleton: React.FC = () => {
         </div>
 
         <div className="flex flex-col gap-1 sm:flex-row sm:items-center sm:gap-3">
-          <Skeleton className="h-4 w-32" />
           <Skeleton className="h-4 w-24" />
+          <Skeleton className="h-4 w-32" />
           <Skeleton className="h-4 w-28" />
         </div>
       </div>
 
-      <div className="mt-2 flex items-center gap-2 sm:mt-0">
-        <Skeleton className="h-9 w-28" />
+      <div className="mt-2 sm:mt-0">
+        <Skeleton className="h-9 w-32" />
       </div>
     </Card>
   );
-};
-
-export default WaiverSignatureSkeleton;
+}
